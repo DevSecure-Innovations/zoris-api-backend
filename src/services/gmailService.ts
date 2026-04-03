ad2be9a322077b67980b8b632e7364f104786df1
SUPRØ <00youareworngtho00@gmail.com>
2026-04-03 20:57:01 +0530

    feat(gmailServices): add correct client credentials as param

--- src/services/gmailService.ts
+++ src/services/gmailService.ts
@@ -1,13 +1,22 @@
-import { oauth2Client, gmail } from '../config/google';
+import { google } from 'googleapis';
+import type { OAuth2Client } from 'google-auth-library';
 
 const GCP_PROJECT_ID = process.env.GCP_PROJECT_ID;
 
 if (!GCP_PROJECT_ID) throw new Error('Missing GCP_PROJECT_ID');
 
-export async function setupGmailWatch(accessToken: string) {
-	oauth2Client.setCredentials({ access_token: accessToken });
+export async function setupGmailWatch(client: OAuth2Client) {
+	const gmail = google.gmail({
+		version: 'v1',
+		auth: client, 
+	});
 
-	await gmail.users.stop({ userId: 'me' });
+	// Stop previous watch (optional but good practice)
+	try {
+		await gmail.users.stop({ userId: 'me' });
+	} catch (err) {
+		// ignore if no existing watch
+	}
 
 	const res = await gmail.users.watch({
 		userId: 'me',
